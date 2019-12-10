d499fc79b77c1a37a769341f45b0eff37557bbd2
mdlugo2016 <mdlugo2016@gmail.com>
2019-12-10 15:59:57 -0500

    first commit

---
+++ build/precache-manifest.cfa165dd3dc3e3c850a64b4f063809a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62441e8fbc8e64fcf41573bc88b6ad27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eeccbdc0f0e082638b7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9386138d766d2bbede8",
+    "url": "/static/js/2.9747f88b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9747f88b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eeccbdc0f0e082638b7",
+    "url": "/static/js/main.4da50977.chunk.js"
+  },
+  {
+    "revision": "16c9cb75b31716913895",
+    "url": "/static/js/runtime-main.d2f9f56d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
